900fce10c41153256b8669cb1e071e8d56f4a788
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-07-11 13:35:59 +0900

    Add widget file

---
+++ qa-similar-tag-widget.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+	Question2Answer Plugin: Similar Tag Widget
+*/
+
+class qa_similar_tag_widget {
+
+	function allow_template($template)
+	{
+		$allow=false;
+
+		return $allow;
+	}
+
+	function allow_region($region)
+	{
+		$allow=false;
+
+		return $allow;
+	}
+
+	function output_widget($region, $place, $themeobject, $template, $request, $qa_content)
+	{
+
+	}
+}
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
